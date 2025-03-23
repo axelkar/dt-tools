8c201447aece549fd04a1a719abab95c95b7ddcf
Axel Karjalainen <axel@axka.fi>
2025-03-24 00:42:16 +0200

    chore: update Nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "dt-tools devel";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nixpkgs, flake-utils, ... }:
@@ -9,9 +9,9 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShellNoCC {
           env.RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
-          packages = [ pkgs.llvmPackages.bintools ];
+          nativeBuildInputs = [ pkgs.llvmPackages.bintools ];
         };
       });
 }
